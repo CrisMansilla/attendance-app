628846fd540a586ae3cd16ffa404489bac315af9
Cristian <cr.mansilla@duocuc.cl>
2025-10-26 03:31:00 -0300

    Revert app service and server port back to 3000 in Docker Compose and index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const layouts = require('express-ejs-layouts');
 const path = require('path');
 const app = express();
-const port = 8081;
+const port = 3000;
 
 // Set EJS as the view engine
 app.set('view engine', 'ejs');
